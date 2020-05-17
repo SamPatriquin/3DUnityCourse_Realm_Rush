3ed712426861eb2dba31d8e2fc5981a86edb2b47
SamPatriquin <49535194+SamPatriquin@users.noreply.github.com>
2020-05-16 21:14:13 -0400

    Labels updating with grid pos

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour {
+public class CubeEditor : MonoBehaviour {
     [SerializeField][Range(0, 20)] float gridSnap = 10f;
     // Start is called before the first frame update
     void Start()
@@ -14,11 +14,13 @@ public class EditorSnap : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        TextMesh textMesh = GetComponentInChildren<TextMesh>();
         Vector3 snapPos;
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSnap) * gridSnap;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSnap) * gridSnap;
         snapPos.y = 0f;
 
         transform.position = snapPos;
+        textMesh.text = (snapPos.x/gridSnap + "," + snapPos.z/gridSnap);
     }
 }
