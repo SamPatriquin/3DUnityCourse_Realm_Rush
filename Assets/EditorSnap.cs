3021799f7448973348fd9894cd617764742f03b1
SamPatriquin <49535194+SamPatriquin@users.noreply.github.com>
2020-05-16 20:47:08 -0400

    Cube prefab out of quads. GridSnapping script

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour {
+    [SerializeField][Range(0, 20)] float gridSnap = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSnap) * gridSnap;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSnap) * gridSnap;
+        snapPos.y = 0f;
+
+        transform.position = snapPos;
+    }
+}
