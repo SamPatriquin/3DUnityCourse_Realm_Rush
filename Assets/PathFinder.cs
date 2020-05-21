3a20496d36f6a20b52318d96bc9fd302434c72b4
SamPatriquin <49535194+SamPatriquin@users.noreply.github.com>
2020-05-20 20:55:12 -0400

    pathFinder script to hold dictionary of the world

---
+++ Assets/PathFinder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour
+{
+    Dictionary<Vector2Int, Cube> grid = new Dictionary<Vector2Int, Cube>();
+    void Start() {
+        loadBlocks();
+    }
+
+    // Update is called once per frame
+    void Update() {
+      
+    }
+
+    private void loadBlocks() {
+        var cubes = FindObjectsOfType<Cube>();
+
+        foreach (Cube cube in cubes) {
+            if (grid.ContainsKey(cube.getGridPos())) {
+                Debug.LogWarning("Overlapping block: " + cube);
+            } else {
+                grid.Add(cube.getGridPos(), cube);
+            }
+        }
+    }
+}
